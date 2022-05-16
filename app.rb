2fec27763741aa48ca4276ef0adeb25a91138c60
Erin Van Brunt <erinmontybruce@gmail.com>
2022-05-16 10:52:58 -0700

    done 2

--- app.rb
+++ app.rb
@@ -1,19 +1,33 @@
-# app.rb
-# puts "Hello world! 😇"
-# puts "Hello world! 😇"
-# puts "Hello world! 😇"
+# puts 'Hello World!'
+# puts 'Hello World!'
+# puts 'Hello World!'
 
+# print 'Hello World!🍓'
+# print 'Hello World!🍓'
+# print 'Hello World!🍓'
 
-# print "Hello world!👽"
-# print "Hello world!👽"
-# print "Hello world!👽"
+# pp 'Hello World!🎈'
+# pp 'Hello World!🎈'
+# pp 'Hello World!🎈'
 
 # puts [1, 2, 3]
-# print [1, 2, 3]
+# p [3, 4, 5]
+# pp [6, 7, 8]
 
-# pp [{id: 1, hello: "World"}, {id: 2, hello: "Ruby"}, {id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+# pp [
+#      { id: 1, hello: 'World' },
+#      { id: 2, hello: 'Ruby' },
+#      { id: 3, hello: 'Moon' },
+#      { id: 4, hello: 'Learner' },
+#    ]
 
+# print [
+#         { id: 1, hello: 'World' },
+#         { id: 2, hello: 'Ruby' },
+#         { id: 3, hello: 'Moon' },
+#         { id: 4, hello: 'Learner' },
+#       ]
 
-puts "Hello World!"
-print "Pass this test, please."
+puts 'Hello World!'
+print 'Pass this test, please.'
 p [1, 2, 3]
